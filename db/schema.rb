f814fc659da0227bc06ef72156084cb8ded4c2da
Paula <paulaproxy@gmail.com>
2012-07-03 02:39:34 +0200

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120703001525) do
+
+  create_table "links", :force => true do |t|
+    t.string   "address"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
 end
